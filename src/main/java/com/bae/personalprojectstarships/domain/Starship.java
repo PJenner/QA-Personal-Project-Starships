05444e2b7e001d57e4e9ccb64f857c9321dd34a4
Peter Jenner <peterdavidjenner@gmail.com>
2021-03-08 18:09:09 +0000

    added Starship class

---
+++ src/main/java/com/bae/personalprojectstarships/domain/Starship.java
@@ -0,0 +1,67 @@
+package com.bae.personalprojectstarships.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Starship {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; // <- Type of id
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	private String name;
+	private String model;
+	private int age;
+
+	public Starship(String name, String model, int age) {
+		this.name = name;
+		this.model = model;
+		this.age = age;
+	}
+
+	public Starship(Long id, String name, String model, int age) {
+		this.id = id;
+		this.name = name;
+		this.model = model;
+		this.age = age;
+	}
+
+	public Starship() {
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
