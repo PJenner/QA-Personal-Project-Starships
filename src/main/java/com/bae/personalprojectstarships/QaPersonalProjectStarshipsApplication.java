5332ebba5008237585c4e6f5cc29b83b4b7fcf25
Peter Jenner <peterdavidjenner@gmail.com>
2021-03-08 17:59:49 +0000

    first commit

---
+++ src/main/java/com/bae/personalprojectstarships/QaPersonalProjectStarshipsApplication.java
@@ -0,0 +1,13 @@
+package com.bae.personalprojectstarships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaPersonalProjectStarshipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaPersonalProjectStarshipsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bae/personalprojectstarships/QaPersonalProjectStarshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bae.personalprojectstarships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaPersonalProjectStarshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
