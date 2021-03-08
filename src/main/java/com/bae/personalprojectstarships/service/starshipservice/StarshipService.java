f13569a6c267a1d1ec757b98608ecea49d36af71
Peter Jenner <peterdavidjenner@gmail.com>
2021-03-08 18:19:24 +0000

    added StarshipService

---
+++ src/main/java/com/bae/personalprojectstarships/service/starshipservice/StarshipService.java
@@ -0,0 +1,13 @@
+package com.bae.personalprojectstarships.service.starshipservice;
+
+import java.util.List;
+
+import com.bae.personalprojectstarships.domain.Starship;
+
+public interface StarshipService {
+
+	Starship createStarship(Starship starship);
+
+	List<Starship> getAllStarships();
+
+}
