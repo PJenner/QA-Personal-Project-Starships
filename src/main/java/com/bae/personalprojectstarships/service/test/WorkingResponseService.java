bdd3baf4a2a1d4ea19254054b873ef98746f82b9
Peter Jenner <peterdavidjenner@gmail.com>
2021-03-09 19:31:18 +0000

    added service tests and StarshipServiceDBUnitTest

---
+++ src/main/java/com/bae/personalprojectstarships/service/test/WorkingResponseService.java
@@ -0,0 +1,10 @@
+package com.bae.personalprojectstarships.service.test;
+
+public class WorkingResponseService implements ResponseService {
+
+	@Override
+	public String generateResponse() {
+		return "Hello, World!";
+	}
+
+}
